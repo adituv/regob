2eb42c510e0db71d4a00ff549967ad0ea01e52a1
Iris Ward <aditu.venyhandottir@gmail.com>
2016-08-01 22:54:25 +0100

    Rearrange project slightly

--- regob/src/main.cpp
+++ regob/src/main.cpp
@@ -8,6 +8,8 @@
 #include "GobHeader.h"
 
 int main(int argc, const char* argv[]) {
+	auto args = parseArgs(argc, argv);
+
 	if (argc < 2) {
 		std::cerr << "No file given.";
 		return 1;
@@ -20,18 +22,11 @@ int main(int argc, const char* argv[]) {
 	std::unique_ptr<GobHeader> header(GobHeader::FromStream(headerData));
 	headerFile.close();
 
-	auto files = header->GetFileChunks();
-
-	std::cout << std::hex << std::setfill('0');
-
-	std::for_each(files.cbegin(), files.cend(), [](std::vector<ChunkHeader> file) {
-		std::for_each(file.cbegin(), file.cend(), [](ChunkHeader chunk) {
-			std::cout << std::setw(8) << chunk.unk001 << ", ";
-		});
-		std::cout << std::endl << std::endl;
-	});
-
-	std::cin.get();
 
 	return 0;
+}
+
+
+int parseArgs(int argc, const char* argv[]) {
+
 }
